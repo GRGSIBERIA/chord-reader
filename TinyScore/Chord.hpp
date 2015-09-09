66fd53aabd281bdefac2a8bda56a43c9dfa80478
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-09-09 16:03:42 +0900

    三和音と四和音を追加

--- TinyScore/Chord.hpp
+++ TinyScore/Chord.hpp
@@ -5,13 +5,26 @@
 
 namespace cr
 {
+	// コードのベースクラス
 	class Chord
 	{
-		std::wstring chordName;	//!< コード名
-		chord::Tone tone;		//!< メジャー/マイナー
+		std::wstring chordName;	// コード名
+		chord::Tone tone;		// メジャー/マイナー
 
 	public:
 		Chord(const std::wstring& chordName)
 			: chordName(chordName) {}
+	};
+
+	// 三和音
+	class Triad : public Chord
+	{
+
+	};
+
+	// 四和音
+	class Tetrad : public Chord
+	{
+
 	};
 }
