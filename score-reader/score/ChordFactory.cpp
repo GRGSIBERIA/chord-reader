d7cf5bc328809e3ecc71cf3253aaeb8d90f2982d
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-09-10 21:45:39 +0900

    名前の変更

---
+++ score-reader/score/ChordFactory.cpp
@@ -0,0 +1,9 @@
+#include "ChordFactory.hpp"
+#include <regex>
+
+score::ChordPtr score::ChordFactory::MakeChord(const std::wstring& chordName)
+{
+	auto ch = score::ChordPtr(new score::Chord(chordName));
+	
+	return ch;
+}
--- score-reader/score/Recognizer.cpp
+++
@@ -1,9 +0,0 @@
-#include "Recognizer.hpp"
-#include <regex>
-
-score::ChordPtr score::chord::Recognizer::MakeChord(const std::wstring& chordName)
-{
-	auto ch = score::ChordPtr(new score::Chord(chordName));
-	
-	return ch;
-}
