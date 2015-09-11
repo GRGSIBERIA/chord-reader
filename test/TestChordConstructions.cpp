7f9eb45afd219cb1e3c470daa1b1a77f995ac7af
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-09-11 15:21:40 +0900

    名前を変更

--- test/TestChordConstructions.cpp
+++ test/TestChordConstructions.cpp
@@ -1,7 +1,7 @@
 #include "gtest\gtest.h"
 #include <score\ChordConstruction.hpp>
 
-TEST(TestChordRegex, instance)
+TEST(TestChordConstructions, instance)
 {
 	const score::chord::ChordConstructions regs;
 	EXPECT_EQ(std::regex_match(L"Db", regs.roots[0].Regex()), true);
