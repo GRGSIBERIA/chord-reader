bf930fa0bb64346699e60a8134d35bbdc4a7b44a
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-09-11 17:08:42 +0900

    テストの追加

--- test/TestChordConstructions.cpp
+++ test/TestChordConstructions.cpp
@@ -1,8 +1,19 @@
 #include "gtest\gtest.h"
 #include <score\ChordConstruction.hpp>
 
-TEST(TestChordConstructions, instance)
+TEST(TestChordConstructions, root_regex)
 {
 	const score::chord::ChordConstructions regs;
-	EXPECT_EQ(std::regex_match(L"Db", regs.roots[0].Regex()), true);
+	const auto re = regs.roots[0].Regex();
+	EXPECT_EQ(std::regex_match(L"Db", re), true);
+	EXPECT_EQ(std::regex_match(L"Dbm7", re), true);
+	EXPECT_EQ(std::regex_match(L"DbM7", re), true);
+	EXPECT_EQ(std::regex_match(L"DbM7 on C", re), true);
+}
+
+TEST(TestChordConstructions, root_match)
+{
+	const score::chord::ChordConstructions regs;
+	const auto& Db = regs.MatchRoots(L"Db");
+	EXPECT_EQ(Db.Name(), L"Db");
 }
