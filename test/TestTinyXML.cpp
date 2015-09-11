570c9963f5b7ddf609ea6f71e39da75396ca3f8f
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-09-11 13:20:57 +0900

    XMLのテストを修正

--- test/TestTinyXML.cpp
+++ test/TestTinyXML.cpp
@@ -4,5 +4,5 @@
 TEST(TestTinyXML, read)
 {
 	tinyxml2::XMLDocument doc;
-	EXPECT_EQ(doc.LoadFile("autumn leaves.xml"), tinyxml2::XMLError::XML_SUCCESS);
+	EXPECT_EQ(doc.LoadFile("../autumn leaves.xml"), tinyxml2::XMLError::XML_SUCCESS);
 }
