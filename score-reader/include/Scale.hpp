311ebdfe8e9933d2d28146057e684ccf77cd59ee
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-09-14 00:52:07 +0900

    各種スケールを実装

--- score-reader/include/Scale.hpp
+++ score-reader/include/Scale.hpp
@@ -13,13 +13,19 @@ namespace score
 			ScaleBase(const std::vector<int>& scale) : scale(scale) {}
 		};
 
-#define DEFINE_SCALE(CLASS_NAME, PARAM) \
+#define DEFINE_SCALE(CLASS_NAME, C, D, E, F, G, A, B) \
 		class CLASS_NAME : public ScaleBase\
 		{\
 		public:\
-			CLASS_NAME() : ScaleBase(PARAM) {}\
-		};
+			CLASS_NAME() : ScaleBase({C, D, E, F, G, A, B}) {}\
+		}
 
-		DEFINE_SCALE(Ionian, {});
+		DEFINE_SCALE(Ionian, 0, 2, 4, 5, 7, 9, 11);
+		DEFINE_SCALE(Dorian, 0, 2, 3, 5, 7, 9, 10);
+		DEFINE_SCALE(Phrygian, 0, 1, 3, 5, 7, 8, 10);
+		DEFINE_SCALE(Lydian, 0, 2, 4, 6, 7, 9, 11);
+		DEFINE_SCALE(Mixolydian, 0, 2, 4, 5, 7, 9, 10);
+		DEFINE_SCALE(Aeolian, 0, 2, 3, 5, 7, 8, 10);
+		DEFINE_SCALE(Locrian, 0, 1, 3, 5, 6, 8, 10);
 	}
 }
