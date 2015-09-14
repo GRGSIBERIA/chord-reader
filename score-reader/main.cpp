5c4a5f0c26205852fe17cc8592cb955b7ee0988a
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-09-14 16:51:33 +0900

    なんかうるさい

--- score-reader/main.cpp
+++ score-reader/main.cpp
@@ -4,11 +4,6 @@
 
 void main()
 {
-	std::wstring str(L"C");
-	const score::chord::ChordConstructions re;
-	if (std::regex_match(str, re.roots[0].Regex()))
-	{
-		printf("test\n");
-	}
+	
 	system("pause");
 }
