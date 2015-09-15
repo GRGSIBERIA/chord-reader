62bf509765713c506a5be19c170815c90184ecae
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-09-16 00:44:25 +0900

    ほげ

--- score-reader/include/DiatonicDatabase.hpp
+++ score-reader/include/DiatonicDatabase.hpp
@@ -7,7 +7,7 @@ namespace score
 	{
 		class DiatonicDatabase
 		{
-			const std::shared_ptr<DiatonicDatabase> _ptr;
+			static std::shared_ptr<DiatonicDatabase> _ptr;
 			DiatonicDatabase() {}
 		public:
 			static const DiatonicDatabase& GetInstance();
