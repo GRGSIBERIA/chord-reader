9d99d0a589fb4c16d823621cfedfdacbd2a74f63
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-09-15 18:08:22 +0900

    ほげ

--- score-reader/include/ModeDatabase.hpp
+++ score-reader/include/ModeDatabase.hpp
@@ -5,18 +5,22 @@ namespace score
 {
 	namespace scale
 	{
+		/**
+		* モードスケール用のデータベース
+		* シングルトンクラス
+		*/
 		class ModeDatabase
 		{
 			typedef std::vector<Mode> Modes;
 
 			const Modes modes = Modes{ {
-				Mode(L"Ionian", 0, { 0, 2, 4, 5, 7, 9, 11 }, { 5 }),
-				Mode(L"Dorian", 1, { 0, 2, 3, 5, 7, 9, 10 }, { 9 }),
-				Mode(L"Phrygian", 2, { 0, 1, 3, 5, 7, 8, 10 }, { 1, 8 }),
-				Mode(L"Lydian", 3, { 0, 2, 4, 6, 7, 9, 11 }, {}),
+				Mode(L"Ionian",		0, { 0, 2, 4, 5, 7, 9, 11 }, { 5 }),
+				Mode(L"Dorian",		1, { 0, 2, 3, 5, 7, 9, 10 }, { 9 }),
+				Mode(L"Phrygian",	2, { 0, 1, 3, 5, 7, 8, 10 }, { 1, 8 }),
+				Mode(L"Lydian",		3, { 0, 2, 4, 6, 7, 9, 11 }, {}),
 				Mode(L"Mixolydian", 4, { 0, 2, 4, 5, 7, 9, 10 }, { 5 }),
-				Mode(L"Aeolian", 5, { 0, 2, 3, 5, 7, 8, 10 }, { 9 }),
-				Mode(L"Locrian", 6, { 0, 1, 3, 5, 6, 8, 10 }, { 1, 8 })
+				Mode(L"Aeolian",	5, { 0, 2, 3, 5, 7, 8, 10 }, { 9 }),
+				Mode(L"Locrian",	6, { 0, 1, 3, 5, 6, 8, 10 }, { 1, 8 })
 					} };
 
 			static std::shared_ptr<ModeDatabase> _ptr;
