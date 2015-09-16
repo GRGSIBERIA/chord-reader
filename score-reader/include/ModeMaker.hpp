d9d98fa747c20f78218f7ab0038078a98d8b9daa
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-09-16 22:43:48 +0900

    ModeMakerクラスを生成

---
+++ score-reader/include/ModeMaker.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "Scale.hpp"
+
+namespace score
+{
+	namespace scale
+	{
+		/**
+		* スケールからモードを自動生成するためのクラス
+		*/
+		class ModeMaker
+		{
+		public:
+			ModeMaker(const Scale& scale)
+			{
+
+			}
+		};
+	}
+}
