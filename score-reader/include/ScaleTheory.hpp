1c8eefe20e426713c0e54e48a6148860a236cb32
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-09-20 01:52:38 +0900

    スケールの自動生成処理を追加

--- score-reader/include/ScaleTheory.hpp
+++ score-reader/include/ScaleTheory.hpp
@@ -8,14 +8,26 @@ namespace score
 	{
 		typedef std::vector<int> ScaleIntervals;
 		typedef std::vector<ScaleIntervals> ScaleIntervalArray;
-		typedef std::shared_ptr<ScaleIntervalArray> ScaleIntervalArrayPtr;
 
+		/**
+		* スケール理論クラス
+		*/
 		class ScaleTheory
 		{
 			ScaleIntervalArray intervalArray;
+			const size_t numberOfModal = 12;	// 12音階
+			const size_t modalsInScale;			// スケール内の音階数
+
+			void BuildScale(const size_t i);
 
 		public:
+			/**
+			* 入力インターバルから，それぞれのインターバルより，スケールを自動生成する
+			*/
 			ScaleTheory(const ScaleIntervals& intervals);
+
+			const size_t NumberOfModal() const { return numberOfModal; }
+			const ScaleIntervals& GetScale(const size_t modal) const { return intervalArray[modal]; }
 		};
 	}
 }
--- score-reader/src/ScaleTheory.cpp
+++ score-reader/src/ScaleTheory.cpp
@@ -1,7 +1,27 @@
 #include "ScaleTheory.hpp"
+#include <algorithm>
 using namespace score::scale;
 
-ScaleTheory::ScaleTheory(const ScaleIntervals& intervals) : intervalArray(12, ScaleIntervals(intervals.size())) 
+void ScaleTheory::BuildScale(const size_t i)
 {
+	auto& scale = intervalArray[i];
 
+	for (size_t j = 0; j < modalsInScale; ++j)
+	{
+		scale[j] = intervalArray[0][j] + (int)i;
+		if (scale[j] >= numberOfModal) scale[j] -= numberOfModal;
+	}
+
+	std::sort(scale.begin(), scale.end());	// 適当に数字を大きくしてからソートする
+}
+
+ScaleTheory::ScaleTheory(const ScaleIntervals& intervals) : intervalArray(numberOfModal, ScaleIntervals(intervals.size())), modalsInScale(intervals.size())
+{
+	intervalArray[0] = intervals;
+
+	for (size_t i = 1; i < numberOfModal; ++i)
+	{
+		size_t tmp_i = i >= numberOfModal ? i - numberOfModal : i;
+		BuildScale(tmp_i);
+	}
 }
