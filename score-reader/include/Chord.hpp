df6b957b6d43c8a1a6fc7aa2d0931092ede6886d
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-09-19 18:10:59 +0900

    コードトーンを取得できるようにする

--- score-reader/include/Chord.hpp
+++ score-reader/include/Chord.hpp
@@ -14,7 +14,8 @@ namespace score
 		// コードのベースクラス
 		class Chord
 		{
-			std::wstring chordName;	// コード名
+			const std::wstring chordName;	// コード名
+			const std::wstring chordTone; // コードトーン名
 
 		public:
 			friend ChordFactory;
@@ -27,12 +28,13 @@ namespace score
 			const OnChord& onchord;
 
 			Chord(const std::wstring& chordName, const Root& root, const Tone& tone, const Fifth& fifth, const Dominant& dominant, const Tension& tension, const OnChord& onchord)
-				: chordName(chordName), root(root), tone(tone), fifth(fifth), dominant(dominant), tension(tension), onchord(onchord) {}
+				: chordName(chordName), root(root), tone(tone), fifth(fifth), dominant(dominant), tension(tension), onchord(onchord), chordTone(tone.Name() + dominant.Name() + fifth.Name()) {}
 
-			Chord(const Chord& c) 
-				: chordName(c.chordName), root(c.root), tone(c.tone), fifth(c.fifth), dominant(c.dominant), tension(c.tension), onchord(c.onchord) {}
+			Chord(const Chord& c)
+				: chordName(c.chordName), root(c.root), tone(c.tone), fifth(c.fifth), dominant(c.dominant), tension(c.tension), onchord(c.onchord), chordTone(c.chordTone) {}
 
 			const std::wstring& Name() const { return chordName; }
+			const std::wstring& ChordTone() const { return chordTone; }
 		};
 
 		typedef std::shared_ptr<Chord> ChordPtr;
