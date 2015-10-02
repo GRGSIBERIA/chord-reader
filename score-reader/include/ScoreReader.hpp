90cf44ed7292bfacec07d17a854ca691e59ff32d
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-10-02 22:06:18 +0900

    ScoreReaderを追加

--- score-reader/include/ScoreReader.hpp
+++ score-reader/include/ScoreReader.hpp
@@ -1,9 +1,17 @@
 #pragma once
+#include <string>
+#include "xml\tinyxml2.h"
 
 namespace score
 {
-	class ScoreReader
+	namespace reader
 	{
+		class ScoreReader
+		{
+			tinyxml2::XMLDocument doc;
 
-	};
+		public:
+			ScoreReader(const std::string& path);
+		};
+	}
 }
---
+++ score-reader/src/ScoreReader.cpp
@@ -0,0 +1,9 @@
+#include "ScoreReader.hpp"
+#include <exception>
+using namespace score::reader;
+
+ScoreReader::ScoreReader(const std::string& path)
+{
+	if (doc.LoadFile(path.c_str()) != tinyxml2::XMLError::XML_SUCCESS)
+		throw std::exception((std::string("File not found: ") + path).c_str());
+}
