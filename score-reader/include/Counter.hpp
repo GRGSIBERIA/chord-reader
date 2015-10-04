dfe646ce84c530427131a752b10cedd60759116c
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-10-04 22:51:45 +0900

    ほげほげ

--- score-reader/include/Counter.hpp
+++ score-reader/include/Counter.hpp
@@ -4,12 +4,25 @@ namespace score
 {
 	namespace score
 	{
-		struct Counter
+		class Counter
 		{
+			const int rythm;	// ”q
+
+		public:
 			size_t partNum;
 			size_t keyNum;
 			size_t measureNum;
 			size_t chordNum;
+			int beat;
+
+			Counter(const int rythm) : rythm(rythm), partNum(0), keyNum(0), measureNum(0), chordNum(0), beat(0) {}
+
+			void Increment()
+			{
+				beat++;
+				if (rythm <= beat)
+					beat = 0;
+			}
 		};
 	}
 }
--- score-reader/include/Measure.hpp
+++ score-reader/include/Measure.hpp
@@ -10,14 +10,18 @@ namespace score
 		class Measure : public ConstructionBase < Measure, chord::Chord >
 		{
 			int count;
+			int tick_per_chord;
+
 		public:
-			Measure(const int count) : count(count) {}
+			Measure(const int count) : count(count), tick_per_chord(4 / count) {}
 
 			const int Count() const { return count; }
 
 			static _PPtr Instantiate(const int count) { return _PPtr(new Measure(count)); }
 
 			const chord::Chord& Chord(const size_t i) const { return *elem[i]; }
+
+			const int BeatPerChord() const { return tick_per_chord; }	// 1ƒR[ƒh‚ ‚½‚è‚Ì””
 		};
 	}
 }
