402fd7d39b4cf6ea058a3d96209a8c02a3291c36
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-10-04 23:37:13 +0900

    多重定義回避

--- score-reader/src/ScoreTurner.cpp
+++ score-reader/src/ScoreTurner.cpp
@@ -2,4 +2,7 @@
 using namespace score::score;
 
 ScoreTurner::ScoreTurner(const Score::_PPtr& score, const int rythm) 
-	: rythm(rythm), partNum(0), keyNum(0), measureNum(0), chordNum(0), beat(0), ptr(score), score(*score) {}
+	: rythm(rythm), partNum(0), keyNum(0), measureNum(0), chordNum(0), beat(0), ptr(score), score(*score) 
+{
+
+}
