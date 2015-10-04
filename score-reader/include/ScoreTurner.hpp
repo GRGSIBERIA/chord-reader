0e638f037673da518c16eed69c9683ffc7fd51e3
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-10-04 23:36:24 +0900

    多重定義

--- score-reader/include/ScoreTurner.hpp
+++ score-reader/include/ScoreTurner.hpp
@@ -20,7 +20,7 @@ namespace score
 			const Score& score;
 
 		public:
-			ScoreTurner(const Score::_PPtr& score, const int rythm) : rythm(rythm), partNum(0), keyNum(0), measureNum(0), chordNum(0), beat(0), ptr(score), score(*score) {}
+			ScoreTurner(const Score::_PPtr& score, const int rythm);
 		};
 	}
 }
