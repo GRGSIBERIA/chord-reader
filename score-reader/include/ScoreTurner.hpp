464108338e014e0a8150d1bdf0f436ca1112a4a1
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-10-04 22:56:26 +0900

    譜めくりクラスを作る

---
+++ score-reader/include/ScoreTurner.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include "Score.hpp"
+
+namespace score
+{
+	namespace score
+	{
+		class ScoreTurner
+		{
+			const int rythm;	// îèéq
+
+			size_t partNum;
+			size_t keyNum;
+			size_t measureNum;
+			size_t chordNum;
+			int beat;
+
+			const Score::_PPtr ptr;
+			const Score& score;
+
+		public:
+			ScoreTurner(const Score::_PPtr& score, const int rythm) : rythm(rythm), partNum(0), keyNum(0), measureNum(0), chordNum(0), beat(0), ptr(score), score(*score) {}
+		};
+	}
+}
---
+++ score-reader/src/ScoreTurner.cpp
@@ -0,0 +1,5 @@
+#include "ScoreTurner.hpp"
+using namespace score::score;
+
+ScoreTurner::ScoreTurner(const Score::_PPtr& score, const int rythm) 
+	: rythm(rythm), partNum(0), keyNum(0), measureNum(0), chordNum(0), beat(0), ptr(score), score(*score) {}
