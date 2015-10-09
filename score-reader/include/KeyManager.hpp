8b35145c2bd86da970d0a93b0e9dc91568a98f73
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-10-09 17:32:00 +0900

    マネージャーさん

---
+++ score-reader/include/KeyManager.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "KeyChordModalizer.hpp"
+
+namespace score
+{
+	namespace score
+	{
+		class KeyManager : public std::vector<scale::KeyChordModalizer>
+		{
+		public:
+			// キーから追加
+			void PushBack(const std::wstring& key) { 
+				if (std::find_if(begin(), end(), [key](const scale::KeyChordModalizer& m) { return key == m.KeyName(); }) == end())
+					emplace_back(key);
+			}
+			
+			// キーから探す
+			const scale::KeyChordModalizer& Find(const std::wstring& key) const { 
+				return *std::find_if(begin(), end(), [key](const scale::KeyChordModalizer& m) { return key == m.KeyName(); }); 
+			}
+		};
+	}
+}
