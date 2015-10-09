52ddd10bae69d12138c63e27ee40f38c360ea131
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-10-09 21:38:35 +0900

    コメント追加

--- score-reader/include/Key.hpp
+++ score-reader/include/Key.hpp
@@ -8,14 +8,14 @@ namespace score
 		class Key
 		{
 		public:
-			static const std::wstring Parallel(const std::wstring& str);
-			static const std::wstring Relative(const std::wstring& str);
+			static const std::wstring Parallel(const std::wstring& str);	// “¯å’²(I)
+			static const std::wstring Relative(const std::wstring& str);	// •½s’²(maj=VIm, min=III)
 
-			static const std::wstring Dominant(const std::wstring& str);
-			static const std::wstring MinorDominant(const std::wstring& str);
+			static const std::wstring Dominant(const std::wstring& str);		// ‘®’²(V)
+			static const std::wstring MinorDominant(const std::wstring& str);	// ‘®’²2(maj=IIIm, min=VII)
 
-			static const std::wstring SubDominant(const std::wstring& str);
-			static const std::wstring MinorSubDominant(const std::wstring& str);
+			static const std::wstring SubDominant(const std::wstring& str);			// ‰º‘®’²(IV)
+			static const std::wstring MinorSubDominant(const std::wstring& str);	// ‰º‘®’²2(maj=IIm, min=VI)
 		};
 	}
 }
