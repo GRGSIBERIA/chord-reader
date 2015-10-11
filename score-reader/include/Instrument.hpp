4e81ed7ab3643163385c075d76f06767dbeb13f8
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-10-11 15:40:38 +0900

    適当な便利関数を作る

--- score-reader/include/Instrument.hpp
+++ score-reader/include/Instrument.hpp
@@ -19,6 +19,12 @@ namespace score
 			}
 
 			GET_PROPERTY(const std::vector<String>&, Strings, strings);
+
+			// MIDIノート番号を返す
+			const MidiNoteNumber MidiNum(const int stringNum, const int fretNum) const { return strings[stringNum].Tuning + fretNum; }
+
+			// フレット番号を返す，マイナスの場合は存在しない
+			const int FretNum(const int stringNum, const MidiNoteNumber midiNoteNum) const { return midiNoteNum - strings[stringNum].Tuning; }
 		};
 	}
 }
