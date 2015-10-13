b540d501cd78e3e2e66956af8edce7acde8dbc79
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-10-13 23:20:06 +0900

    コメント整理

--- score-reader/include/Modulation.hpp
+++ score-reader/include/Modulation.hpp
@@ -26,11 +26,11 @@ namespace score
 		class Modulation
 		{
 		public:
-			static const std::wstring Parallel(const std::wstring& key);	// “¯å’²(I)
-			static const std::wstring Relative(const std::wstring& key);	// •½s’²(maj=VIm, min=III)
-			static const std::wstring Dominant(const std::wstring& key);		// ‘®’²(V)
+			static const std::wstring Parallel(const std::wstring& key);			// “¯å’²(I)
+			static const std::wstring Relative(const std::wstring& key);			// •½s’²(maj=VIm, min=III)
+			static const std::wstring Dominant(const std::wstring& key);			// ‘®’²(V)
 			static const std::wstring SubDominant(const std::wstring& key);			// ‰º‘®’²(IV)
-			static const std::wstring MinorDominant(const std::wstring& key);	// ‘®’²2(maj=IIIm, min=VII)
+			static const std::wstring MinorDominant(const std::wstring& key);		// ‘®’²2(maj=IIIm, min=VII)
 			static const std::wstring MinorSubDominant(const std::wstring& key);	// ‰º‘®’²2(maj=IIm, min=VI)
 		};
 	}
