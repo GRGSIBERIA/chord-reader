dac2fd0500185690eb14139092c0959f6c274e9a
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-10-13 23:17:33 +0900

    コードの仕様を変更

--- score-reader/include/ChordScale.hpp
+++ score-reader/include/ChordScale.hpp
@@ -18,14 +18,15 @@ namespace score
 			const int modeIndex;
 			const chord::Chord& chord;
 			const ModeModals& scale;
+			const ModeModals& available;
 		public:
 			GET_PROPERTY(const int, ScaleIndex, scaleIndex);
 			GET_PROPERTY(const chord::Chord&, Chord, chord);
 			GET_PROPERTY(const ModeModals&, Scale, scale);
 			GET_PROPERTY(const int, ModeIndex, modeIndex);
 
-			ChordScale(const chord::Chord& chord, const int scaleIndex, const int modeIndex, const ModeModals& scale)
-				: chord(chord), scale(scale), scaleIndex(scaleIndex), modeIndex(modeIndex) {}
+			ChordScale(const chord::Chord& chord, const int scaleIndex, const int modeIndex, const ModeModals& scale, const ModeModals& available)
+				: chord(chord), scale(scale), scaleIndex(scaleIndex), modeIndex(modeIndex), available(available) {}
 		};
 	}
 }
